0be0e4c95dbfd0c298507f6c0d68108198efafd5
Manas <manas94gupta@gmail.com>
2016-11-27 16:39:45 +0530

    Add data.js with all the data objects
    Add data.js which contains all the users data. Data from this file will
    be used to build the html and display the info on the resume.

---
+++ js/data.js
@@ -0,0 +1,94 @@
+// Header object
+var header = {
+    "name": "Walter White",
+    "role": "Chemical Engineer",
+    "biopic": "biopic.jpg"
+};
+
+// Personal object
+var personal = {
+    "name": "Walter White",
+    "sex": "Male",
+    "dateofbirth": "Jan 1, 1956",
+    "nationality": "American",
+    "languages": "English"
+};
+
+// Contacts object
+var contacts = {
+    "mobile": "9213467816",
+    "email": "heisenberg@ww.com",
+    "address": "25th Street, Albuquerque"
+};
+
+// Links object
+var links = {
+    "github": "walterwhite",
+    "linkedin": "walterwhite",
+    "twitter": "heisenberg",
+    "website": "heisenberg.com"
+};
+
+// Profile object
+var profile = {
+    "profile": "Lorem ipsum dolor sit amet, pro ut eirmod fabellas, has ea aliquam disputationi moderatius quaerendum per. No periculis democritum concludaturque pri, et sonet moderatius quaerendum per, eum ea mollis albucius mnesarchum. Cu splendide voluptatum sed. Cu duo persius assueverit, ut sit elit persius euismod."
+};
+
+// Skills object
+var skills = {
+    "skills": [
+        "Chemist", "Entrepreneur", "Budgeting", "Crisis Management"
+    ]
+};
+
+// Experience object
+var experience = {
+    "jobs": [
+        {
+            "date": "Feb, 1979 - Jan, 1984",
+            "title": "Co-Founder",
+            "location": "California",
+            "company": "Gray Matter Technology",
+            "description": "Lorem ipsum dolor sit amet, pro ut eirmod fabellas, has ea aliquam disputationi moderatius quaerendum per. No periculis democritum concludaturque pri, et sonet moderatius quaerendum per, eum ea mollis albucius mnesarchum. Cu splendide voluptatum sed. Cu duo persius assueverit, ut sit elit persius euismod."
+        },
+        {
+            "date": "Jun, 1990 - Dec, 2009",
+            "title": "Chemistry Teacher",
+            "location": "Albuquerque",
+            "company": "JP Wynne High School",
+            "description": "Lorem ipsum dolor sit amet, pro ut eirmod fabellas, has ea aliquam disputationi moderatius quaerendum per. No periculis democritum concludaturque pri, et sonet moderatius quaerendum per, eum ea mollis albucius mnesarchum. Cu splendide voluptatum sed. Cu duo persius assueverit, ut sit elit persius euismod."
+        }
+    ]
+};
+
+// Projects object
+var projects = {
+    "projects": [
+        {
+            "date": "1981",
+            "title": "Proton Radiography",
+            "description": "Lorem ipsum dolor sit amet, pro ut eirmod fabellas, has ea aliquam disputationi moderatius quaerendum per. No periculis democritum concludaturque pri, et sonet moderatius quaerendum per, eum ea mollis albucius mnesarchum. Cu splendide voluptatum sed. Cu duo persius assueverit, ut sit elit persius euismod."
+        },
+        {
+            "date": "2007",
+            "title": "Blue Meth",
+            "description": "Lorem ipsum dolor sit amet, pro ut eirmod fabellas, has ea aliquam disputationi moderatius quaerendum per. No periculis democritum concludaturque pri, et sonet moderatius quaerendum per, eum ea mollis albucius mnesarchum. Cu splendide voluptatum sed. Cu duo persius assueverit, ut sit elit persius euismod."
+        }
+    ]
+};
+
+// Education object
+var education = {
+    "schools": [
+        {
+            "date": "Aug, 1974 - Jun, 1978",
+            "degree": "Chemical Engineering and Applied Sciences",
+            "college": "California Institute of Technology"
+        },
+        {
+            "date": "Mar, 1972 - Jun, 1974",
+            "degree": "High School",
+            "college": "JP Wynne High School"
+        }
+    ]
+};
